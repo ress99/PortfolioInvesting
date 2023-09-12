8f1c1363cf06cc242054929b1afdea8437539781
ress99 <mressurreicao99@gmail.com>
2023-09-12 23:06:29 +0100

    added gitignore to folder

--- sp500.py
+++ sp500.py
@@ -1,33 +1,43 @@
-import time
 from ticker import Ticker
 import data_op as op
+ 
 
-'''start = time.time()
-end = time.time()   
-print(end - start)'''   
-
+#####
+#Class used to 
 class SP500:
 
-
+    #####
+    #Saves ticker inside the dictionary self.sp500_dict
+    #####
     def get_ticker(self, ticker_name):
 
         ticker_name = ticker_name.upper()
 
+        #If the ticker_name exists
         if ticker_name in self.tickers_list:
+            #If the ticker is not yet in self.sp500_dict, initialize it
             if self.sp500_dict[ticker_name] is None:
                 self.sp500_dict[ticker_name] = Ticker(ticker_name)
             return self.sp500_dict[ticker_name]
 
+        #If the ticker_name is not valid
         else:
             print('Ticker name not found')
             return
 
-
+    #####
+    #Creates a dictionary where all the Ticker objects
+    #Can be saved and accessed
+    #####
     def create_sp500_dict(self):
 
+        #Creates and empty list the size of the sp500
         empty_list = [None] * len(self.tickers_list)
+
+        #Zip the tickers names and the empty list
         zipped = zip(self.tickers_list, empty_list)
 
+        #Return as dictionary
         return dict(zipped)
 
 
@@ -37,18 +47,3 @@ class SP500:
         self.sp500_dict = self.create_sp500_dict()
 
 
-
-sp500 = SP500()
-
-listt = sp500.tickers_list
-
-'''start = time.time()
-j = 0
-for i in listt:
-    starttt = time.time()
-    sp500.get_ticker(i)
-    enddd = time.time() 
-    print(enddd - starttt, j)
-    j +=1
-end = time.time()   
-print(end - start)'''
--- ticker.py
+++ ticker.py
@@ -1,21 +1,20 @@
 import pandas as pd
-import time
 import datetime as dt
 
-
-
-
-
-
-
+#####
+#Class to save the information regarding company stock values
+#And used to calculate various stock-related indicators
+#####
 class Ticker:
 
+    #Class attributes
     data_folder = 'Data/'
     initial_columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']
     quote_columns = initial_columns[1:-1]
 
     #####
-    #
+    #Reads the data from the excel file and
+    #Saves it in self.data
     #####
     def save_all_data(self):
 
@@ -25,44 +24,55 @@ class Ticker:
         return data
     
 
-    def get_historical_data(self, column = None):
-      
-        if column == None:                                                  #If nothing is specified, return whole dataframe
-            return self.data[self.initial_columns]
+    #####
+    #Returns the data between a start and end date
+    #If not specified, it returns the original data.
+    #If a column is specified, only that one and Date columns are returned
+    ##### 
+    def get_interval_data(self, column = None, start_date = None, end_date = None):
         
-        elif column in self.quote_columns:                                       #If the column value is correctly specified, 
-            return self.data[['Date', column]]                                   #Return it with date column
-        
-        else:                                                               #If incorrectly specified, print error and return 
-            print('Column name not available')
-            return
+        #Get original data with all or one column
+        if column == None:
+            data = self.data
+        elif column in self.quote_columns:
+            data = self.data[['Date', column]]  
+        else:
+            print('Column not available')
 
-    def get_interval_data(self, start_date = None, end_date = None):
-
-        data = self.data
-
+        #If any start date was specified, remove any data before that 
         if start_date is not None:
             start_date_time = dt.datetime.strptime(start_date, "%Y-%m-%d").date()
             data = data.loc[data['Date'] >= start_date_time]
 
+        #If any start date was specified, remove any data after that 
         if end_date is not None:
             end_date_time = dt.datetime.strptime(end_date, "%Y-%m-%d").date()
             data = data.loc[data['Date'] <= end_date_time]
         
         return data
 
+    #####
+    #Returns the stock return between start_date and end_date
+    #If these are not specified, the first and last day are used
+    #The column on which to calculate the returns can also be specified.
+    #It is set as 'Close' by default
+    #####
     def stock_return(self, column = 'Close', start_date = None, end_date = None):
 
+        #In case of an invalid column
         if column not in Ticker.quote_columns:
             print ('Variable column is not valid')
             return
 
+        #Gets the data on which to calculate the return
         data = self.get_interval_data(start_date, end_date)
 
+        #If there is no data, return
         if data.empty:
-            print('There is no data between these two dates. Check if they were assigned in the right order.')
+            print('There is no data between these two dates. They might have been assigned in the wrong order.')
             return
 
+        #Get first and last value and calculate the return with it
         first_value = data.iloc[0][column]
         last_value = data.iloc[-1][column]
         stock_return = 100 * (last_value - first_value) / first_value
@@ -73,6 +83,7 @@ class Ticker:
 
     def __init__(self, ticker_name):
 
+        #Initializes ticker parameters
         self.ticker_name = ticker_name
         self.filename = self.data_folder + 'Historical Data/' + self.ticker_name + '.xlsx'
         self.data = self.save_all_data()
