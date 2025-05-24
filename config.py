4e1e44b1726e715dde1695f36877ab22d8d84db7
ress99 <mressurreicao99@gmail.com>
2025-05-24 17:12:13 +0100

    update on config settings

--- config.py
+++ config.py
@@ -1,17 +1,19 @@
+"""Module with software configurations"""
 
 filetype = '.csv'
-logging_level = 'WARNING'
+LOG_LEVEL = 'INFO'
 data_folder = 'Data'
 hist_folder = 'Historical'
 financial_folder = 'Financial'
 prtf_folder = 'Portfolios'
-as_folder = 'Asset_Selection'
-po_folder = 'Portfolio_Optimization'
+as_folder = 'AssetSelection'
+po_folder = 'PortfolioOptimization'
 test_folder = 'Test_Run'
 assets_folder = 'Assets'
+validate_attributes = True
 
 first_day = '2010-01-01'
 last_day = '2021-01-01'
 
 pop_size = 100
-size_cache = 250
+size_cache = 500
