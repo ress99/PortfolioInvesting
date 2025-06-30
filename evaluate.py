cea9817e2d52045a2a46ada3a06f0c450d0911af
ress99 <mressurreicao99@gmail.com>
2025-06-30 16:01:45 +0100

    update changes in evaluate

--- evaluate.py
+++ evaluate.py
@@ -5,12 +5,12 @@ def return_risk(ind):
 
     ind.fitness.values = prtf_return, prtf_variance
 
-def pe_roe(ind):
+# def pe_roe(ind):
 
-    prtf_roe = ind.portfolio_roe()
-    prtf_pe = ind.portfolio_pe()
+#     prtf_roe = ind.portfolio_roe()
+#     prtf_pe = ind.portfolio_pe()
 
-    ind.fitness.values = prtf_roe, prtf_pe
+#     ind.fitness.values = prtf_roe, prtf_pe
 
 def sharpe_var(ind):
 
